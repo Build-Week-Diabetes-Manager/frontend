b475beb8017bdf53894a360cf59655fd711ca04d
zachtyoung <zachyoung@Zachs-MacBook-Pro.local>
2019-08-26 13:09:56 -0500

    Sanity Commit

--- diabetes-management/src/App.js
+++ diabetes-management/src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import FormikUserForm from './components/Login'
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Route exact path="/" component={FormikUserForm} />
     </div>
+    </Router>
   );
 }
 
