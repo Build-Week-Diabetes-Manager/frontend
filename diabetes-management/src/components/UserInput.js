8e554dc2109564c8680a42eeb7ae83751344ea3e
Landry Irakoze <irakozel@gmail.com>
2019-08-28 16:13:50 -0400

    glucose measurement input

--- diabetes-management/src/components/UserInput.js
+++ diabetes-management/src/components/UserInput.js
@@ -1,63 +1,61 @@
 import React, { useState, useEffect } from 'react';
-import { axiosWithAuth } from '../utils/axiosWithAuth';
+import Select from 'react-select';
+import axiosWithAuth from '../utils/axiosWithAuth';
 
-const UserInput = (props) => {
+const glucoseOptions = [
+    { value: 57, label: 'unspecified blood glucose measurement' },
+    { value: 58, label: 'pre-breakfast blood glucose measurement' },
+    { value: 59, label: 'post-breakfast blood glucose measurement' },
+    { value: 60, label: 'pre-lunch blood glucose measurement' },
+    { value: 61, label: 'post-lunch blood glucose measurement' },
+    { value: 62, label: 'pre-supper blood glucose measurement' },
+    { value: 63, label: 'post-supper blood glucose measurement' },
+];
 
-    const [userInfo, setUserInfo] = useState({ 
-        userTime: false,
-        timeStamp: "",
-        glucoseMeasurement: "",
-        regularDose: "",     
-        nphDose: "",   
-        ultralenteDose: "",
-    })
+const insulineType = [
+    {value: 33, label: 'Regular insuline dose'},
+    {value: 34, label: 'NPH insuline dose'},
+    {value: 35, label: 'UltraLente insuline dose'},
+]
 
-    // const [userInfo, setUserInfo] = useState({ 
-    //     timeStamp: "",
-    //     glucoseMeasurement: "",
-    //     insulineType: {
-    //         regularDose: "",     
-    //         nphDose: "",   
-    //         ultralenteDose: "", 
-    //     }
-    // })
+const Form = () => {
 
-    const handleChange = event => {
-        setUserInfo({...userInfo, [event.target.name]: event.target.value})
-    }
+    const [glucose, setGlucose] = useState({ timestamp: '', code: '', value: ''});
+    const [dose, setDose] = useState({ timestamp: '', code: '', value: ''});
+
+    const handleGlucoseSelect = selectedOption => {
+        setGlucose({...glucose, code: selectedOption.value });
+        console.log(`Option selected:`, selectedOption);
+    };
+
+    const handleGlucoseChange = event => {
+        setGlucose({ ...glucose, value: parseInt(event.target.value) })
+    };
 
     const handleSubmit = event => {
         event.preventDefault();
-        axiosWithAuth()
-            .post()
-            .then()
-            .catch()
+        setGlucose({...glucose, timestamp: currentTime()})
+        console.log(glucose);
+    }
+
+    const currentTime = () => {
+        const time = new Date().toISOString();
+        return(`${time.substr(0,10)} ${time.substr(11,5)}`);
     }
 
     return (
-        <>
-            <form onSubmit={event => handleSubmit(event)}>
-                <label>
-                    Time:
-                    <input name="userTime" value={userInfo.userTime} onChange={event => handleChange(event)} />
-                </label>
-                <legend>Insuline Dosage</legend>
-                <label>
-                    Regular:
-                    <input name="regularDose" value={userInfo.regularDose} onChange={event => handleChange(event)} />
-                </label>
-                <label>
-                    NPH:
-                    <input name="nphDose" value={userInfo.nphDose} onChange={event => handleChange(event)} />
-                </label>
-                <label>
-                    UltraLente:
-                    <input name="ultralenteDose" value={userInfo.ultralenteDose} onChange={event => handleChange(event)} />
-                </label>
-                <button>Submit</button>
-            </form>
-        </>
+        <form onSubmit={event => handleSubmit(event)}>
+            <label>Glucose Measurement Time</label>
+            <Select
+                value={glucose.code}
+                onChange={event => handleGlucoseSelect(event)}
+                options={glucoseOptions}
+            />
+            <label>Glucose Measurement</label>
+            <input type="number" name="glucose measurement" onChange={event => handleGlucoseChange(event)} value={glucose.value} />
+            <button type="submit">Submit</button>
+        </form>
     )
 }
 
-export default UserInput;
+export default Form;
