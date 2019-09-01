e630e2239a271be4c567d5925db7654b0083063a
zachtyoung <zachyoung@Zachs-MacBook-Pro.local>
2019-09-01 04:24:44 -0500

    Sanity Commit

--- diabetes-management/src/components/NutriList.js
+++ diabetes-management/src/components/NutriList.js
@@ -17,30 +17,54 @@ export default function NutriList() {
     const [food, setFood] = useState([])
 
     const [searchValue, setSearchValue] = useState({searchTerm:"",})
-    console.log("SearchValue", searchValue);
+const [error, setError]= useState(null)
 
    const handleSearchInputChange = (event) => {
-       setSearchValue({...searchValue, [event.target.name] : event.target.value });
+       setSearchValue({searchTerm : event.target.value });
     }
 
-    const resetInputField = () => {
-        setSearchValue("");
-    }
+    // .get(`https://api.nal.usda.gov/ndb/V2/reports?ndbno=${props.food.ndbno}&type=f&format=json&api_key=dCI2jG9Xjje6T5rvhUL09LQFpjRn5zM67aLtYIu7`)
+    // const callSearchFunction = (event) => {
+    //     event.preventDefault();
+
+    //     axios
+    //     .get(`https://api.nal.usda.gov/ndb/search/?format=json&q=${searchValue.searchTerm}&sort=n&offset=0&api_key=dCI2jG9Xjje6T5rvhUL09LQFpjRn5zM67aLtYIu7 
+    //     `)
+    //     .then(res => {
+    //         setFood(res.data.list.item)
+    //     })
+        
+    // }
+    // const callSearchFunction = (event) => {
+    //     event.preventDefault();
+
+    //     axios.get(`https://api.nal.usda.gov/ndb/search/?format=json&q=${searchValue.searchTerm}&sort=n&offset=0&api_key=dCI2jG9Xjje6T5rvhUL09LQFpjRn5zM67aLtYIu7`)
+    //     .then((response) => {
+    //       return response.data.list.item.map(el => axios.get(`https://api.nal.usda.gov/ndb/V2/reports?ndbno=${el.ndbno}&type=f&format=json&api_key=dCI2jG9Xjje6T5rvhUL09LQFpjRn5zM67aLtYIu7`) .then(res => {console.log(res)})); // using response.data
+    //     })
+   
+        
+    // }
 
     const callSearchFunction = (event) => {
         event.preventDefault();
-        
-        axios
-        .get(`https://api.nal.usda.gov/ndb/search/?format=json&q=${searchValue.searchTerm}&sort=n&max=2&offset=0&api_key=dCI2jG9Xjje6T5rvhUL09LQFpjRn5zM67aLtYIu7 
-        `)
-        .then(res => {
-            console.log(res.data.list);
-            setFood(res.data.list.item)
-        })
 
-        .catch( err => {
-            console.log("Axios Err:", err)
-        }, [])
+        axios.get(`https://api.nal.usda.gov/ndb/search/?format=json&q=${searchValue.searchTerm}&sort=n&offset=0&api_key=dCI2jG9Xjje6T5rvhUL09LQFpjRn5zM67aLtYIu7`)
+        .then(async response => {
+            const newArr = response.data.list.item.map(el => el.ndbno)
+            
+          const res = await axios.get(`https://api.nal.usda.gov/ndb/V2/reports?ndbno=${newArr.join("&ndbno=")}&max=10&type=f&format=json&api_key=dCI2jG9Xjje6T5rvhUL09LQFpjRn5zM67aLtYIu7`);
+            console.log(res.data.foods);
+            setFood(res.data.foods)
+            setError(null) // using response.data
+        })
+        .catch(err => {
+            if(err){
+                setError('Please enter a valid item')
+            }
+        }
+         )
+   
         
     }
    
@@ -52,16 +76,15 @@ export default function NutriList() {
         <h2>Food Finder </h2>
         <p> Use our food finder to look up basic nutrient values</p>
         <div className="search-bar"> 
-        <form className="search">
+        <form className="search" onSubmit={callSearchFunction}>
             <input name='searchTerm' value={searchValue.searchTerm} onChange={handleSearchInputChange} type="text" />
-            <input className="button-style-1" onClick={callSearchFunction} type="submit" value="SEARCH" />
+            <input className="button-style-1" type="submit" value="SEARCH" />
         </form>
         </div>
 
             <div className="nutriList">
-                {food.map(el => (
-                <NutriCard key={el.id} item={el} ndbno={el.ndbno} />
-                ))}
+                {error? <h2 className='errorText'>Please be more specific</h2> :food.map(el => <NutriCard key={el.food.desc.ndbno} food={el} /> )}
+               
             </div> 
 
         </>
