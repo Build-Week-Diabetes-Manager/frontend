9f028363d7b1275990919d0c019a38f353e96e9c
Zach Young <zachyoung@Zachs-MacBook-Pro.local>
2020-07-01 04:24:37 -0500

    WIP

--- src/components/charts/ChartTwo.js
+++ src/components/charts/ChartTwo.js
@@ -8,66 +8,17 @@ const sampleUserData = [80, 90, 70, 64, 123];
 
 export const TestLine = (props) => {
 	const [newData, setNewData] = useState({});
+	const [userData, setUserData] = useState([]);
+	const [predData, setPredData] = useState([]);
+
+
 	const [updateData, setUpdateData] = useState();
 	const id = localStorage.getItem("user_id");
-	console.log(`id: ${id}`);
+	// console.log(`id: ${id}`);
 
 	const glucoseLineChart = () => {
 		console.log("Ping");
-		setNewData({
-			labels:[
-				'pre-breakfast blood glucose measurement',
-				'post-breakfast blood glucose measurement',
-				'pre-lunch blood glucose measurement',
-				'post-lunch blood glucose measurement',
-				'pre-supper blood glucose measurement',
-				'post-supper blood glucose measurement',
-			],
-			datasets: [
-				{
-					label: "Predicted Glucose Levels",
-					fill: false,
-					lineTension: 0.5,
-					backgroundColor: colors.skyblue,
-					borderColor: colors.skyblue,
-					borderCapStyle: "butt",
-					borderDash: [],
-					borderDashOffset: 1,
-					borderJoinStyle: "miter",
-					pointBorderColor: "rgba(75,192,192,1)",
-					pointBackgroundColor: "#fff",
-					pointBorderWidth: 1,
-					pointHoverRadius: 5,
-					pointHoverBackgroundColor: "rgba(75,192,192,1)",
-					pointHoverBorderColor: "rgba(220,220,220,1)",
-					pointHoverBorderWidth: 2,
-					pointRadius: 1,
-					pointHitRadius: 10,
-					data: testData,
-				},
-				{
-					label: `User's Glucose Level`,
-					fill: false,
-					lineTension: 0.1,
-					backgroundColor: "#666",
-					borderColor: "#666",
-					borderCapStyle: "butt",
-					borderDash: [],
-					borderDashOffset: 0.0,
-					borderJoinStyle: "miter",
-					pointBorderColor: "#666",
-					pointBackgroundColor: "#fff",
-					pointBorderWidth: 1,
-					pointHoverRadius: 5,
-					pointHoverBackgroundColor: "#666",
-					pointHoverBorderColor: "rgba(220,220,220,1)",
-					pointHoverBorderWidth: 2,
-					pointRadius: 1,
-					pointHitRadius: 10,
-					data: sampleUserData,
-				},
-			],
-		});
+
 	};
 
 	useEffect(() => {
@@ -75,25 +26,85 @@ export const TestLine = (props) => {
 	}, [updateData]);
 
 	useEffect(() => {
+		
+		let emptyUser = [];
+		let emptyPred = [];
 		console.log("Pre API Call", newData)
 		axiosWithAuth()
-			.get(`https://diabetesmanager.herokuapp.com/api/manager/manage/ds/1`)
-			.then((res) => {
+			.get(`https://diabetesmanager.herokuapp.com/api/manager/ds/${id}`)
+			.then( (res) => {
 				console.log("ds response", res);
-				setNewData(newData, (newData.datasets[0].data = Object.values(res.data)));
+				let temp = Object.values(res.data)
+				temp.forEach(el =>{
+					emptyPred.push(Math.floor(el))
+				})
+				axiosWithAuth()
+				.get(`https://diabetesmanager.herokuapp.com/api/manager/${id}`)
+				.then((res) => {
+					// console.log("user data", res);
+					console.log("After API call", newData);
+					res.data.forEach(el => {
+						emptyUser.push(el.value)
+					});
+					setNewData({
+						labels:[
+							'pre-breakfast blood glucose measurement',
+							'post-breakfast blood glucose measurement',
+							'pre-lunch blood glucose measurement',
+							'post-lunch blood glucose measurement',
+							'pre-supper blood glucose measurement',
+							'post-supper blood glucose measurement',
+						],
+						datasets: [
+							{
+								label: "Predicted Glucose Levels",
+								fill: false,
+								lineTension: 0.5,
+								backgroundColor: colors.skyblue,
+								borderColor: colors.skyblue,
+								borderCapStyle: "butt",
+								borderDash: [],
+								borderDashOffset: 1,
+								borderJoinStyle: "miter",
+								pointBorderColor: "rgba(75,192,192,1)",
+								pointBackgroundColor: "#fff",
+								pointBorderWidth: 1,
+								pointHoverRadius: 5,
+								pointHoverBackgroundColor: "rgba(75,192,192,1)",
+								pointHoverBorderColor: "rgba(220,220,220,1)",
+								pointHoverBorderWidth: 2,
+								pointRadius: 1,
+								pointHitRadius: 10,
+								data: emptyPred,
+							},
+							{
+								label: `User's Glucose Level`,
+								fill: false,
+								lineTension: 0.1,
+								backgroundColor: "#666",
+								borderColor: "#666",
+								borderCapStyle: "butt",
+								borderDash: [],
+								borderDashOffset: 0.0,
+								borderJoinStyle: "miter",
+								pointBorderColor: "#666",
+								pointBackgroundColor: "#fff",
+								pointBorderWidth: 1,
+								pointHoverRadius: 5,
+								pointHoverBackgroundColor: "#666",
+								pointHoverBorderColor: "rgba(220,220,220,1)",
+								pointHoverBorderWidth: 2,
+								pointRadius: 1,
+								pointHitRadius: 10,
+								data: emptyUser,
+							},
+						],
+					});
+				})
+				.catch((err) => console.log("axios err: ", err));
 			})
 			.catch((err) => console.log("axios err: ", err));
-		axiosWithAuth()
-			.get(`https://diabetesmanager.herokuapp.com/api/manager/manage/${id}`)
-			.then((res) => {
-				// console.log("user data", res);
-				console.log("After API call", newData);
-				setNewData(
-					newData,
-					(newData.datasets[1].data = res.data.map((el) => el.value))
-				);
-			})
-			.catch((err) => console.log("axios err: ", err));
+
 	}, []);
 
 
