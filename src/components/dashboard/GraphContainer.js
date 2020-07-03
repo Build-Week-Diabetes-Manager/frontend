6eb80be5e5542ff6806996765ee2ed3226435ae8
Zach Young <zachyoung@Zachs-MacBook-Pro.local>
2020-07-03 03:28:37 -0500

    styling

--- src/components/dashboard/GraphContainer.js
+++ src/components/dashboard/GraphContainer.js
@@ -1,24 +1,28 @@
 import { Grid } from "@material-ui/core";
-import React from "react";
+import React,{useState} from "react";
 import TestDoughnut from "../charts/ChartThree";
 import TestLine from "../charts/ChartTwo.js";
 import UserInput from "../glucoseLevelInputForm/UserInput";
+import Paper from '@material-ui/core/Paper';
+
+
 
 
 const GraphContainer = () => {
-
 	return (
-		<Grid container spacing={2} >
+		<Grid container className="root" spacing={2} >
 			<Grid item className="graphs graph-one" sm={12} lg={6}>
-				<TestDoughnut />
+			<Paper className="paper"><TestDoughnut /></Paper>
+				
 			</Grid>
 
 			<Grid item className="graphs graph-two "sm={12} lg={6}>
-				<TestLine />
+			<Paper className="paper"><TestLine /></Paper>
 			</Grid>
 
 			<Grid item className="user-input" lg={12}>
-				<h2>Blood Glucose Level Input </h2>
+			<Paper className="paper">
+			<h2>Blood Glucose Level Input </h2>
 				<h4>
 					{" "}
 					Please input your glucose measurement and the time you took the measurement
@@ -26,6 +30,8 @@ const GraphContainer = () => {
 				</h4>
 				<p className=""> </p>
 				<UserInput />
+			</Paper>
+
 			</Grid>
 		</Grid>
 	);
